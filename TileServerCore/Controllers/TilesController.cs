1f1da890d44ba3453a09f7f8ea98ba888b190282
reilly.wood <reilly.wood@icloud.com>
2018-02-13 14:17:55 -0800

    initial SQLite functionality

--- TileServerCore/Controllers/TilesController.cs
+++ TileServerCore/Controllers/TilesController.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
 
 namespace TileServerCore.Controllers
 {
     [Route("api/[controller]")]
     public class TilesController : Controller
     {
-        // GET api/tiles
-        [HttpGet]
-        public IEnumerable<string> Get()
+        // GET tiles/z/x/y
+        [HttpGet("{z}/{x}/{y}")]
+        public byte[] Get(int z, int x, int y)
         {
-            return new string[] { "value1", "value2" };
+            var connectionStringBuilder = new SqliteConnectionStringBuilder
+            {
+                DataSource = @"%HOME%\tiles\parcels.mbtiles"
+            };
+
+            using (var connection = new SqliteConnection(connectionStringBuilder.ToString()))
+            {
+                connection.Open();
+
+                using (var transaction = connection.BeginTransaction())
+                {
+                    var selectCommand = connection.CreateCommand();
+                    selectCommand.Transaction = transaction;
+                    selectCommand.CommandText = $"SELECT tile_data FROM `tiles` where zoom_level = {z} and tile_column = {x} and tile_row = {y}";
+                    var tileData = (byte[]) selectCommand.ExecuteScalar();
+                    return tileData;
+                }
+            }
+            
         }
 
         // GET api/values/5
